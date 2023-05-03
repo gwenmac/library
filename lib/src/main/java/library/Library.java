8f6969a1aaaa2866aa5797bb81a91edf986a88e2
gwenmac <gwenmac522@gmail.com>
2023-05-03 15:09:53 -0400

    Separated controller and application

--- lib/src/main/java/library/Library.java
+++ lib/src/main/java/library/Library.java
@@ -3,25 +3,13 @@
  */
 package library;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+
 public class Library {
     public static void main(String[] args) {
         SpringApplication.run(Library.class, args);
-    }
-
-    @GetMapping("/")
-    public String hello() {
-        return String.format("Hello!");
     }
 }
